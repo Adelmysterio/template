fa988e41555b4824225b0a918a9f7b02656a57fc
AdelMysterio <storliniadelmo@gmail.com>
2024-07-16 10:01:08 +0200

    completato template

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,3 @@
-<header class="container">
-    <nav>
-        <figure><img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo DC"></figure>
-        <ul>
-            <li>
-                character
-            </li>
-            <li>
-                comics
-            </li>
-            <li>
-                movies
-            </li>
-        </ul>
-    </nav>
+<header>
+
 </header>
