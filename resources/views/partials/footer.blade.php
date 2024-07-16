ad5b77f3114a84547b376b0db50683b09de1cdf8
Adelmysterio <164235569+Adelmysterio@users.noreply.github.com>
2024-07-16 09:50:23 +0200

    Initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,32 @@
+<footer>
+    <div class="container">
+        <nav>
+            <section>
+                <h3>dc comics</h3>
+                <ul class="mb-1">
+                    <li></li>
+                </ul>
+                <h3>shop</h3>
+                <ul>
+                    <li></li>
+                </ul>
+            </section>
+            <section>
+                <h3>dc</h3>
+                <ul>
+                    <li></li>
+                </ul>
+            </section>
+            <section>
+                <h3>sites</h3>
+                <ul>
+                    <li></li>
+                </ul>
+            </section>
+        </nav>
+        <figure>
+            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+        </figure>
+    </div>
+
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header class="container">
+    <nav>
+        <figure><img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo DC"></figure>
+        <ul>
+            <li>
+                character
+            </li>
+            <li>
+                comics
+            </li>
+            <li>
+                movies
+            </li>
+        </ul>
+    </nav>
+</header>
